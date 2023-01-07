66b900f6b098ad72a618049f70566c237b4f3246
MIN <alsrb4171@gmail.com>
2023-01-07 22:52:22 +0900

    Initial commit

---
+++ src/Functions/onReady/Ready.ts
@@ -0,0 +1,10 @@
+import Bot from "../../Bot/Bot";
+import Event from "../../Structures/Event";
+
+function onCall(bot: Bot): void {
+    console.log(`Logged in ${bot.user?.tag}`);
+}
+
+const Ready = new Event("ready", onCall);
+
+export default Ready;
